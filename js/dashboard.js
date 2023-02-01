391445eea2d3e7ab08b0051d72b6af6a89ea0d82
RohanVaghasiya-Tatvasoft <123551487+RohanVaghasiya-Tatvasoft@users.noreply.github.com>
2023-02-01 08:03:18 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,10 +1,1 @@
-const targetDiv = document.getElementById("side-bar");
-const btn = document.getElementById("menubar-logo");
-btn.onclick = function () {
-    if (targetDiv.style.display !== "none") {
-        targetDiv.style.display = "none";
-        
-    } else {
-        targetDiv.style.display = "block";
-    }
-};
+
